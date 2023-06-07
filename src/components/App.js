78bf1cdca2679f100dd1c8a17ea80da4cda13fc6
Sergey Gromov <looneyxx@yandex.ru>
2023-06-07 07:08:13 +0300

    v 0.2

--- src/components/App.js
+++ src/components/App.js
@@ -2,52 +2,184 @@ import './App.css';
 import Header from './Header';
 import React, { useEffect } from 'react';
 import Letter from './Letter';
-import Word from './Word';
-import { text } from '../contexts/CurrentText';
+import { texts } from '../contexts/CurrentText';
 function App() {
     const [currentText, setCurrentText] = React.useState('');
     const [current, setCurrent] = React.useState({ count: 0, symbol: '' });
+    const [isPlaying, setPlaying] = React.useState(false);
+    const [statistics, setStatistics] = React.useState({ well: 0, miss: 0, bpm: 0, accuracy: 0 });
+    const [timer, setTimer] = React.useState(0);
+    const startGame = React.useCallback(
+        function startGame(e) {
+            const random = Math.floor(Math.random() * (texts.length - 1 - 1 + 1) + 1);
+            setCurrentText((prev) => (prev ? `${prev} ${texts[random]}`.trim() : texts[random]));
+
+            setStatistics({ well: 0, miss: 0, bpm: 0, accuracy: 100 });
+            setCurrent((prev) => ({ count: 0, symbol: currentText[prev.count] }));
+            setPlaying(true);
+            setTimer(0);
+        },
+        [currentText]
+    );
 
     useEffect(() => {
-        setCurrent((prev) => ({ ...prev, symbol: currentText[current.count] }));
-    }, [current.symbol, currentText, current.count]);
+        if (isPlaying) {
+            console.log(isPlaying);
+            if (currentText.length > 200) {
+                console.log(200);
+                return;
+            }
 
-    const startGame = React.useCallback(
-        function startGame() {
-            setCurrentText(text);
-            setCurrent({ count: 0, symbol: '' });
+            const random = Math.floor(Math.random() * (texts.length - 1 - 1 + 1) + 1);
+
+            setCurrentText((prev) => (prev ? `${prev} ${texts[random]}`.trim() : texts[random]));
+            // setCurrentText((prev) => `${prev} ${texts[random]}`);
+            // }
+        } else {
+            setCurrentText('');
+        }
+    }, [isPlaying, currentText]);
+    function endGame() {
+        setPlaying(false);
+    }
+    useEffect(() => {
+        if (current.count === currentText.length) {
+            endGame();
+        }
+    });
+    useEffect(() => {
+        setStatistics((prev) => ({ ...prev, bpm: (60 / timer) * prev.well }));
+    }, [timer]);
+    useEffect(() => {
+        setStatistics((prev) => ({
+            ...prev,
+            accuracy: prev.well ? (prev.well * 100) / (prev.well + prev.miss) : 100,
+        }));
+    }, [statistics.miss, statistics.well]);
+    useEffect(() => {
+        let timerId = setInterval(() => {
+            setTimer((prev) => prev + 1);
+        }, 1000);
+
+        if (!isPlaying) {
+            clearInterval(timerId);
+        }
+
+        return () => {
+            clearInterval(timerId);
+        };
+    }, [isPlaying]);
+
+    useEffect(() => {
+        if (currentText) {
+            setCurrent((prev) => ({
+                ...prev,
+                symbol: currentText[current.count] === ' ' ? '_' : currentText[current.count],
+            }));
+        }
+    }, [currentText, current.count]);
+
+    const isSomeSymbols = React.useCallback(function isSomeSymbols(symbol) {
+        return symbol === 'Shift' || symbol === 'Alt' || symbol === 'Control' || symbol === 'Escape';
+    }, []);
+    const isBackspace = React.useCallback(
+        (symbol) => {
+            return symbol === ' ' && current.symbol === '_';
         },
-        [text]
+        [current.symbol]
     );
 
     const getCurrentKey = React.useCallback(
         (e) => {
-            console.log(current);
-            if (current.symbol === e.key) {
-                setCurrent((prev) => ({ count: prev.count + 1 }));
+            if ((current.symbol === e.key && !isSomeSymbols(e.key)) || isBackspace(e.key)) {
+                setCurrent((prev) => ({ count: prev.count + 1, symbol: currentText[prev.count + 1] }));
+                setStatistics((prev) => ({ ...prev, well: prev.well + 1 }));
+                return;
             }
-            return e.key;
+
+            if (e.key === 'Escape') {
+                setPlaying(false);
+            }
+            if (isSomeSymbols(e.key)) {
+                return;
+            }
+
+            setStatistics((prev) => ({ ...prev, miss: prev.miss + 1 }));
         },
-        [current]
+        [currentText, isSomeSymbols, current.symbol, isBackspace]
     );
     useEffect(() => {
         window.addEventListener('keydown', getCurrentKey);
+        if (!isPlaying) {
+            window.removeEventListener('keydown', getCurrentKey);
+        }
         return () => {
             window.removeEventListener('keydown', getCurrentKey);
         };
-    }, [getCurrentKey]);
+    }, [getCurrentKey, isPlaying]);
+    const getLevelAccuracy = React.useCallback((accuracy) => {
+        if (accuracy > 90) {
+            return 'accuracy-count-number_high';
+        }
+        if (accuracy > 80) {
+            return 'accuracy-count-number_medium';
+        }
+
+        return '';
+    }, []);
+    const getLevelBpm = React.useCallback(function getLevelBpm(bpm) {
+        if (bpm > 100 && bpm < 200) {
+            return 'bpm-count-number_medium';
+        }
+
+        if (bpm > 200) {
+            return 'bpm-count-number_high';
+        }
+
+        return '';
+    }, []);
 
     return (
         <div className='App'>
             <Header />
             <main className='content'>
                 <div className='container'>
-                    <button onClick={startGame} className='button'>
+                    <button
+                        disabled={isPlaying && true}
+                        type='button'
+                        onClick={startGame}
+                        className={`button ${isPlaying && 'button_disabled'}`}
+                    >
                         Начать тренировку
                     </button>
+                    <div className='statistic-group'>
+                        <div className='statistic'>
+                            <p className='current-count'>
+                                Well:<span className='current-count-number'> {statistics.well}</span>
+                            </p>
+                            <p className='miss-count'>
+                                Miss:<span className='miss-count-number'> {statistics.miss}</span>
+                            </p>
+                            <p className='bpm-count'>
+                                BPM:
+                                <span className={`bpm-count-number ${getLevelBpm(statistics.bpm)}`}>
+                                    {' '}
+                                    {statistics.bpm ? statistics.bpm.toFixed(0) : 0}
+                                </span>
+                            </p>
+                            <p className='accuracy-count'>
+                                Accuracy:{' '}
+                                <span className={`accuracy-count-number ${getLevelAccuracy(statistics.accuracy)}`}>
+                                    {statistics.accuracy.toFixed(1)}%
+                                </span>
+                            </p>
+                        </div>
+                        <p className='current'>{current.symbol}</p>
+                    </div>
+
                     <ul className='words-container'>
                         {currentText.split('').map((letter, index) => (
-                            <Letter key={index} letter={letter} isActive={index < current.count} />
+                            <li className='list-letter' key={index}><Letter key={index} letter={letter} isActive={index < current.count} /></li>
                         ))}
                     </ul>
                 </div>
--- src/contexts/CurrentText.jsx
+++ src/contexts/CurrentText.jsx
@@ -1,8 +1,43 @@
-import React from "react";
-
-
 export const text =
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt veniam aspernaturmolestias quasi neque nihil excepturi dignissimos, facilis assumenda ex a idperferendis libero asperiores praesentium pariatur voluptatem esse est tempora. Estlaudantium qui, voluptates laboriosam placeat aspernatur vel ipsa quidem! Cumque fugaqui quae natus iste cupiditate deleniti suscipit!';
 
-
-
+export const texts = [
+    'number',
+    'string',
+    'function',
+    'array',
+    'object',
+    'null',
+    'return',
+    'undefiend',
+    'method',
+    'get',
+    'set',
+    'literal',
+    'expression',
+    'operator',
+    'operand',
+    'for',
+    'while',
+    'length',
+    'map',
+    'each',
+    'filter',
+    'from',
+    'import',
+    'export',
+    'const',
+    'setTimeout',
+    'setInterval',
+    'promise',
+    'async',
+    'await',
+    'reject',
+    'resolve',
+    'responce',
+    'useState',
+    'useCallback',
+    'useEffect',
+    'React',
+    'default',
+];
